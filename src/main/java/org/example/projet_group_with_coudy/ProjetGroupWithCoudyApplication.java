23bf6db873af718c3239bd3cf64e17ad890ff5ce
mansour-niang <mansour.niang4@unchk.edu.sn>
2026-07-25 11:25:44 +0000

    chore: init projet Spring Boot (Java 17, Maven, springdoc-openapi, openapi-generator)

---
+++ src/main/java/org/example/projet_group_with_coudy/ProjetGroupWithCoudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.projet_group_with_coudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGroupWithCoudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetGroupWithCoudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projet_group_with_coudy/ProjetGroupWithCoudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projet_group_with_coudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGroupWithCoudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
