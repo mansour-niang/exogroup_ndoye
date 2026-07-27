3ebcd50d2f9c988493e3a27dc856a49ce6f5f08e
mansour-niang <mansour.niang4@unchk.edu.sn>
2026-07-27 16:12:03 +0000

    feat(besoin2): GREEN - bourse de mobilite (>50km) x coefficient social par palier de revenu

--- src/main/java/org/example/projet_group_with_coudy/engine/FinancingEngine.java
+++ src/main/java/org/example/projet_group_with_coudy/engine/FinancingEngine.java
@@ -16,6 +16,8 @@ public class FinancingEngine {
 
     private static final int SCALE = 2;
     private static final BigDecimal SEUIL_PAUVRETE_REVENU_ANNUEL = new BigDecimal("1200000");
+    private static final BigDecimal DISTANCE_MINIMALE_MOBILITE_KM = new BigDecimal("50");
+    private static final BigDecimal BOURSE_MOBILITE_BASE = new BigDecimal("20000");
 
     private static final Map<StudyCycle, BigDecimal> FRAIS_INSCRIPTION_PAR_CYCLE = new EnumMap<>(StudyCycle.class);
 
@@ -35,7 +37,7 @@ public class FinancingEngine {
 
     public FinancingPlan calculate(StudentApplication application) {
         BigDecimal tuitionFees = calculateTuitionFees(application);
-        BigDecimal grossMonthlyScholarship = BigDecimal.ZERO.setScale(SCALE);
+        BigDecimal grossMonthlyScholarship = calculateMobilityScholarship(application);
         BigDecimal housingAidDeduction = BigDecimal.ZERO.setScale(SCALE);
         BigDecimal monthlyScholarship = grossMonthlyScholarship.subtract(housingAidDeduction);
 
@@ -53,5 +55,26 @@ public class FinancingEngine {
             return BigDecimal.ZERO.setScale(SCALE);
         }
         return FRAIS_INSCRIPTION_PAR_CYCLE.get(application.studyCycle()).setScale(SCALE, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateMobilityScholarship(StudentApplication application) {
+        if (application.homeDistanceKm().compareTo(DISTANCE_MINIMALE_MOBILITE_KM) <= 0) {
+            return BigDecimal.ZERO.setScale(SCALE);
+        }
+        BigDecimal coefficientSocial = calculateSocialCoefficient(application.familyAnnualIncome());
+        return BOURSE_MOBILITE_BASE.multiply(coefficientSocial).setScale(SCALE, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateSocialCoefficient(BigDecimal familyAnnualIncome) {
+        if (familyAnnualIncome.compareTo(SEUIL_PAUVRETE_REVENU_ANNUEL) <= 0) {
+            return new BigDecimal("2.0");
+        }
+        if (familyAnnualIncome.compareTo(SEUIL_PAUVRETE_REVENU_ANNUEL.multiply(BigDecimal.valueOf(2))) <= 0) {
+            return new BigDecimal("1.5");
+        }
+        if (familyAnnualIncome.compareTo(SEUIL_PAUVRETE_REVENU_ANNUEL.multiply(BigDecimal.valueOf(4))) <= 0) {
+            return new BigDecimal("1.0");
+        }
+        return new BigDecimal("0.5");
     }
 }
